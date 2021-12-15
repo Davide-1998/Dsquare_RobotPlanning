f4166082af2324e213e5ef233f5d659977a19811
Davide-1998 <bertellidavide98@gmail.com>
2021-12-15 16:10:28 +0100

    Added argparse possibility

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,1 +1,17 @@
-g++ main.cpp ./Dubins/*.cpp ./Roadmap/*.cpp `pkg-config opencv4 --cflags --libs`
+#! /bin/bash
+
+key="$1"
+
+  case $key in 
+   -v| --version)
+		VERSION="$2"
+     ;;
+    *)
+     VERSION=""
+ 	 ;;
+  esac
+
+out_name="Dsquare_RP.out"
+ 
+g++ -o $out_name main.cpp ./Dubins/*.cpp ./Roadmap/*.cpp `pkg-config opencv$VERSION --cflags --libs`
+./$out_name
